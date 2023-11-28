052458bc40fef57bc8a7c5a8162c6975abb2c4e2
jswwWorks <jsww.works@gmail.com>
2023-11-28 18:42:30 -0500

    Solved serial.py problem.

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,28 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=100):
+        """Initializes starting point for serial numbers"""
+        self.current = start
+
+        # This will be static
+        self.start = start
+
+    def generate(self):
+        """Generate serial number, incrementing by 1."""
+        self.result = self.current
+        self.current += 1
+
+        return self.result
+
+
+
+    def reset(self):
+        """Reset the serial number to initial value."""
+        self.current = self.start
+
+    def __repr__(self):
+        return (f"This is starting point for the serial num {self.start}")
+
+
