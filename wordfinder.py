c7f1dad8a1df7fdbac6df66ba2cea3900595d84d
jswwWorks <jsww.works@gmail.com>
2023-11-28 20:08:01 -0500

    Added docstrings to wordfinder.py and created file with spaces and comments to test filtering ability

--- wordfinder.py
+++ wordfinder.py
@@ -7,16 +7,29 @@ class WordFinder:
 # prints out “[num-of-words-read] words read”
 # method random() returns random word from the list of words we made in line 4
     def __init__(self):
+        """
+        Open file with words, call functions that create and clean word
+        lists, calls function to display words.
+        """
         self.file = open("/usr/share/dict/words", "r")
+        #self.file = open("wordtestfile.txt", "r")
 
-        self.words_list = self.create_words_list()
+        self.unfiltered_words_list = self.create_words_list()
+        self.words_list = self.clean_words_list()
 
         self.file.close()
 
         self.display_words()
 
-    def create_words_list(self):
 
+    def __repr__(self):
+        """Displays properties of WordFinder instance."""
+        print(f"This is the unfiltered word list: {self.unfiltered_words_list}")
+        print(f"This is the filtered word list: {self.words_list}")
+
+
+    def create_words_list(self):
+        """Goes through each line of file and appends line to a list."""
         words_list = []
 
         for word in self.file:
@@ -24,12 +37,29 @@ class WordFinder:
 
         return words_list
 
-    def display_words(self):
+    def clean_words_list(self):
+        """Filters initial word list to eliminate blank lines and comments."""
+        clean_words = []
 
-        print(f"{len(self.words_list)} words read")
+        for line in self.unfiltered_words_list:
+            empty_test = line.strip()
+
+            # If it's empty, this will catch it
+            if bool(empty_test) and not line.startswith("#"):
+                clean_words.append(line)
+
+        return clean_words
+
+
+    def display_words(self):
+        """Prints how many words are in unfiltered and filtered word lists."""
+
+        print(f"{len(self.unfiltered_words_list)} words in unfiltered file")
+        print(f"There are {len(self.words_list)} words in filtered words list")
 
 
     def random(self):
+        """Grabs a random word from words list and strips '\n'"""
         random_word = random.choice(self.words_list)
         return random_word[:-1:1]
 
